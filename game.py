597e6a3e12ee6dc13979c122c354cbf0ec52f948
William Fecteau <william-fecteau@hotmail.com>
2022-06-30 00:07:01 -0400

    State balls

---
+++ game.py
@@ -0,0 +1,35 @@
+import pygame
+from constants import TARGET_FPS
+
+from renderer import Renderer
+from states.gameState import GameState
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.font.init()
+
+        renderer = Renderer()
+
+        self.dicStates = {
+            "game": GameState(self, renderer)
+        }
+        self.curState = "game"
+
+        self.clock = pygame.time.Clock()
+
+    def gameLoop(self):
+        while True:
+            self.dicStates[self.curState].update()
+            self.dicStates[self.curState].render()
+            self.clock.tick(TARGET_FPS)
+
+
+    def switchState(self, newState: str):
+        self.dicStates[self.curState].onExitState()
+        self.curState = newState
+        self.dicStates[self.curState].onEnterState()
+
+
+if __name__ == "__main__":
+    Game().gameLoop()
