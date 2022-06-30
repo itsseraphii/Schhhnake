94ca68375bc3afb1f7741eee615ebfc2ba7c2e0e
Jordan <itsseraphii@gmail.com>
2022-06-30 14:26:00 -0400

    Click button to see ball

--- button.py
+++ button.py
@@ -1,30 +1,34 @@
+from pydoc import cli
 import pygame
 
+from utils import Utils
+
 class Button:
-    def __init__(self, x, y, size, bgColor, textColor, hoverColor, font, text):
-        self.x = x
-        self.y = y
-        self.width = size[0]
-        self.height = size[1]
+    def __init__(self, rectangle: pygame.Rect, centerPos: tuple[int, int], bgColor: tuple[int, int, int], hoverColor: tuple[int, int, int], textColor: tuple[int, int, int], font: pygame.font.Font, text: str, clickAction):
+        self.rect = rectangle
+        self.rect.center = centerPos
         self.bgColor = bgColor
         self.textColor = textColor
         self.hoverColor = hoverColor
         self.font = font
         self.text = text
+        self.clickAction = clickAction
 
-    def Draw(self, screen, mousePos):
-        pygame.draw.rect(screen, self.textColor, (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
-        if (self.IsMouseOver(mousePos)):
-            pygame.draw.rect(screen, self.hoverColor, (self.x, self.y, self.width, self.height), 0)
+    def draw(self, screen: pygame.Surface):
+        pygame.draw.rect(screen, self.textColor, self.rect)
+        if (self.isMouseOver()):
+            pygame.draw.rect(screen, self.hoverColor, self.rect.inflate(-5, -5))
         else:
-            pygame.draw.rect(screen, self.bgColor, (self.x, self.y, self.width, self.height), 0)
+            pygame.draw.rect(screen, self.bgColor, self.rect.inflate(-5, -5))
         
         text = self.font.render(self.text, 1, self.textColor)
-        screen.blit(text, (self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
+        textRect = text.get_rect()
+        textRect.center = self.rect.center
+        screen.blit(text, textRect)
 
-    def IsMouseOver(self, mousePos):
-        if mousePos[0] > self.x and mousePos[0] < self.x + self.width:
-            if mousePos[1] > self.y and mousePos[1] < self.y + self.height:
-                return True
+    def isMouseOver(self):
+        return self.rect.collidepoint(Utils.getMousePos())
 
-        return False
+    def update(self):
+        if (Utils.checkClicked(self.rect)):
+            self.clickAction()
--- states/menuState.py
+++ states/menuState.py
@@ -1,39 +1,33 @@
 import pygame
 from button import Button
-from constants import GREEN_COLOR
+from constants import EMERALD, GREEN_COLOR, HONEYDEW, ZOMP
 
 from renderer import Renderer
+from utils import Utils
 from states.gameState import GameState
-from states.state import State
 
-class MenuState (State): 
+class MenuState (GameState): 
     def __init__(self, game, renderer: Renderer):
         super().__init__(game, renderer)
         self.surf = pygame.Surface(Renderer.SURFACE_SIZE)
         self.bigSnakeFont = pygame.font.Font('./res/SnakeFont.ttf', 72)
         self.smolSnakeFont = pygame.font.Font('./res/SnakeFont.ttf', 24)
         self.extraSmolSnakeFont = pygame.font.Font('./res/SnakeFont.ttf', 12)
-        self.playButton = Button(Renderer.WIDTH /2 - 75, Renderer.HEIGHT / 2 + 150, (150, 50), (255,0,0), (0,255,0), (0,0,255), self.extraSmolSnakeFont, "press to start")
+        playButtonRect = pygame.Rect(0, 0, 150, 50)
+        self.playButton = Button(playButtonRect, (Renderer.WIDTH/2, Renderer.HEIGHT/2+200), EMERALD, ZOMP, HONEYDEW, self.extraSmolSnakeFont, "press to start", self.menuAction)
 
-
-    def draw(self) -> None:
-        self.playButton.Draw(self.surf, Utils.getMousePos())
+    def draw(self) -> pygame.Surface:
+        self.playButton.draw(self.surf)
         self.surf.blit(self.bigSnakeFont.render("Schhhnake", True, GREEN_COLOR), (150, 150))
         self.surf.blit(self.smolSnakeFont.render("A game where a snake eats balls", True, GREEN_COLOR), (150, 350))
         self.surf.blit(self.smolSnakeFont.render("Centering text is hard", True, GREEN_COLOR), (Renderer.WIDTH-450, Renderer.HEIGHT-250))
         self.renderer.drawSurface(self.surf)
-
+        self.renderer.render()
 
     def update(self) -> None:
-        if pygame.event.peek(pygame.MOUSEBUTTONDOWN):
-            self.game.switchState('game')
+        super().update()
+        self.playButton.update()
+
+    def menuAction(self) -> None:
+        self.game.switchState("game")
         
-
-class Utils:
-    def getMousePos():
-        mousePos = pygame.mouse.get_pos()
-        windowSize = pygame.display.get_window_size()
-        return (Utils.regleDeTroisLol(mousePos[0], windowSize[0], Renderer.WIDTH), Utils.regleDeTroisLol(mousePos[1], windowSize[1], Renderer.HEIGHT))
-
-    def regleDeTroisLol(currentValue, currentScreenMax, rendererScreenMax):
-        return currentValue*currentScreenMax/rendererScreenMax
